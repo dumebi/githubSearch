9dd372fb626028d2acaadf5c81d9dd7db011dae3
Jude <dikejude49@gmail.com>
2016-12-26 19:57:06 +0100

    github search

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { ProfileComponent } from './components/profile.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, CommonModule, HttpModule, JsonpModule],
+  declarations: [ AppComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
